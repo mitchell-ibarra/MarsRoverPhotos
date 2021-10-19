3d818913892c36a3ba941c532d82424500ac7a47
mitchell-ibarra <miibarra48@gmail.com>
2021-10-19 13:56:15 -0600

    Add constant for API Key for use in RoverRetrofitService.kt

--- app/src/main/java/com/miibarra/marsroverphotos/data/remote/RoverRetrofitService.kt
+++ app/src/main/java/com/miibarra/marsroverphotos/data/remote/RoverRetrofitService.kt
@@ -3,19 +3,20 @@ package com.miibarra.marsroverphotos.data.remote
 import com.miibarra.marsroverphotos.data.remote.responses.RoverData
 import com.miibarra.marsroverphotos.data.remote.responses.RoverListData
 import com.miibarra.marsroverphotos.data.remote.responses.SolPhotoData
+import com.miibarra.marsroverphotos.utils.Constants.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RoverRetrofitService {
 
-    @GET("rovers?api_key=DEMO_KEY")
+    @GET("rovers?api_key=$API_KEY")
     suspend fun getRoverList(
         @Query("limit") limit: Int,
         @Query("offset") offset: Int
     ): RoverListData
 
-    @GET("rovers/{roverName}/photos?sol=1000&api_key=DEMO_KEY")
+    @GET("rovers/{roverName}/photos?sol=1000&api_key=$API_KEY")
     suspend fun getRoverPhotoListBySol(
         @Path("roverName") roverName: String,
         @Query("numSol") numSol: Int,
@@ -23,17 +24,16 @@ interface RoverRetrofitService {
         @Query("offset") offset: Int
     ): SolPhotoData
 
-    @GET("rovers/{roverName}?api_key=DEMO_KEY")
+    @GET("rovers/{roverName}?api_key=$API_KEY")
     suspend fun getRoverInformationByName(
         @Path("roverName") roverName: String,
         @Query("limit") limit: Int,
         @Query("offset") offset: Int
     ): RoverData
 
-    @GET("rovers/{roverName}/photos?sol=1000&api_key=DEMO_KEY")
+    @GET("rovers/{roverName}/photos?sol=1000&api_key=$API_KEY")
     suspend fun getPhotoDetail(
         @Path("roverName") roverName: String,
         @Query("id") id: Int
     )
-     // TODO Maye need to get manifest data to get max sol to sort by
 }
--- app/src/main/java/com/miibarra/marsroverphotos/utils/Constants.kt
+++ app/src/main/java/com/miibarra/marsroverphotos/utils/Constants.kt
@@ -4,5 +4,7 @@ object Constants {
 
     const val BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/"
 
+    const val API_KEY = "DEMO_KEY"
+
     const val PAGE_SIZE = 15
 }
